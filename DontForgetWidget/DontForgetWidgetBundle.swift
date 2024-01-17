73aab5bea1219af134aab00c3ebc2ef70cf6ad88
Dayeon Moon <0217dayun@naver.com>
2024-01-17 21:30:24 +0900

    [chore][#1] widget extension 추가

---
+++ DontForgetWidget/DontForgetWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DontForgetWidgetBundle.swift
+//  DontForgetWidget
+//
+//  Created by 제나 on 1/17/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DontForgetWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DontForgetWidget()
+    }
+}
