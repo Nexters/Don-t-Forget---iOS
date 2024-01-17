7c1aee50633d649e2ca35215c7f28d85fcb667a6
Dayeon Moon <0217dayun@naver.com>
2024-01-17 21:00:21 +0900

    [chore][#1] 프로젝트 재생성, gitignore, swiftlint

---
+++ DontForget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DontForget
+//
+//  Created by 제나 on 1/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DontForget/DontForgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontForgetApp.swift
+//  DontForget
+//
+//  Created by 제나 on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct DontForgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
