8fe31862a4fa8ff6cda5a88e48ed4d88a6d8fd87
Dayeon Moon <0217dayun@naver.com>
2024-01-26 15:09:42 +0900

    [feat][#2] app icon, color asset 추가

--- DontForget/Sources/Presentations/Extensions/Color+Extension.swift
+++ DontForget/Sources/Presentations/Extensions/Color+Extension.swift
@@ -15,9 +15,26 @@ extension Color {
             self.init(red: red, green: green, blue: blue, opacity: alpha)
     }
 
+    /* DEPRECATED */
     static let neutral1 = Color(hex: 0xFBFAF1)
     static let neutral2 = Color(hex: 0xD0B874)
     
     static let primary1 = Color(hex: 0xEB5234)
     static let primary2 = Color(hex: 0xD93720)
+    /* DEPRECATED */
+    
+    static let gray50 = Color(hex: 0xF8F9FA)
+    static let gray100 = Color(hex: 0xF1F3F5)
+    static let gray200 = Color(hex: 0xE9ECEf)
+    static let gray300 = Color(hex: 0xDEE2E6)
+    static let gray400 = Color(hex: 0xCED4DA)
+    static let gray500 = Color(hex: 0xADB5BD)
+    static let gray600 = Color(hex: 0x868E96)
+    static let gray700 = Color(hex: 0x495057)
+    static let gray800 = Color(hex: 0x343A40)
+    static let gray900 = Color(hex: 0x212529)
+    
+    static let red500 = Color(hex: 0xFF5C26)
+    static let pink500 = Color(hex: 0xEF83AB)
+    static let primary500 = Color(hex: 0x3D82F6)
 }
